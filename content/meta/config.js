57e9357da86fba4e4a2b076b65216067179c441a
Tanmay Balwa <tanmay.balwa@gmail.com>
2019-04-19 15:32:46 +0530

    cleanup

--- content/meta/config.js
+++ content/meta/config.js
@@ -3,30 +3,28 @@ const colors = require("../../src/styles/colors");
 module.exports = {
   siteTitle: "Test blog for GatsbyJS", // <title>
   shortSiteTitle: "Hosted on Netifly", // <title> ending for posts and pages
-  siteDescription: "PersonalBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
+  siteDescription: "test blog is based on GatsbyJS starter.",
+  siteUrl: "https://lucid-pike-e92aa7.netlify.com/",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Tanmay Balwa",
+  authorTwitterAccount: "thebalwa",
   // info
-  infoTitle: "greg lobinski",
-  infoTitleNote: "personal blog",
+  infoTitle: "Tanmay Balwa",
+  infoTitleNote: "test blog",
   // manifest.json
-  manifestName: "PersonalBlog - a blog starter for GatsbyJS",
-  manifestShortName: "PersonalBlog", // max 12 characters
+  manifestName: "Test blog on gatsby",
+  manifestShortName: "TestBlog", // max 12 characters
   manifestStartUrl: "/",
   manifestBackgroundColor: colors.background,
   manifestThemeColor: colors.background,
   manifestDisplay: "standalone",
   // contact
-  contactEmail: "john@doe.com",
+  contactEmail: "test@example.com",
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "https://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/balwa" }
   ]
 };
